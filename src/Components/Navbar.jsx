c291cdd46d56ad9edf3a34d437e330fa46f29c20
Pradeep2888 <pradeeptiwari2888@gmail.com>
2023-05-25 11:01:20 +0530

    changes some routes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
+  const navigate=useNavigate()
   return (
     <div className='navbarbox' >
-        <div><a href='/home' >Home</a></div>
-        <div><a href='/' >Register</a></div>
-        <div><a href='/login' >Login</a></div>
+        <div><a  onClick={()=>navigate("/home")} >Home</a></div>
+        <div><a onClick={()=>navigate("/")} >Register</a></div>
+        <div><a onClick={()=>navigate("/login")}>Login</a></div>
     </div>
   )
 }
